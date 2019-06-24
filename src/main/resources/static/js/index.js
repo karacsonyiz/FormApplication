5f0188c2789ff756242e9d2052611a5873ca858a
bud07205 <Zoltan.Karacsonyi@ksh.hu>
2019-06-24 19:39:22 +0200

    index.html v2

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,32 +1,3 @@
-window.onload = function(){
-    getUser();
-}
-
-function getUser() {
-        fetch("api/user")
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(jsonData) {
-                console.log(jsonData);
-                storage = window.sessionStorage;
-                storage.setItem("username", jsonData.name);
-                storage.setItem("userid", jsonData.id);
-                console.log(storage)
-            }).catch(error => console.log(error));
-}
-
-function setUserName(jsonData){
-    showAdminMenu(jsonData);
-    let username = document.querySelector("#username");
-    username.innerHTML = "Üdvözöllek, " + jsonData.name + "!";
-    let usermenu = document.querySelector("#usermenu");
-    usermenu.innerHTML = "Munkalista";
-    showAdminMenu(jsonData);
-}
-
-
-
 function startNewForm(){
         let storage = window.sessionStorage;
         console.log(storage)
@@ -49,33 +20,6 @@ function startNewForm(){
                       })
                   .catch(error => console.log(error));
 }
-
-function handleCreateUser() {
-    let name = document.querySelector("#user-name").value;
-    let password = document.querySelector("#user-password").value;
-    let user = {
-                   "name": name,
-                   "password": password,
-               }
-     fetch("api/createuser", {
-                 method: "POST",
-                 headers: {
-                     "Content-Type": "application/json; charset=utf-8"
-                         },
-                 body: JSON.stringify(user)
-        }).then(function(response) {
-                   return response.json();
-               })
-               .then(function(jsonData) {
-                   console.log(jsonData);
-               })
-               .catch(error => console.log(error));
-           return false;
-}
-
-
-
-
 
 $(function() {
     $('[data-toggle="tooltip"]').tooltip()
